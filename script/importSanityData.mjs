2e7373fe06c7128a184aad7596167362efad091f
nousheeniqbal <165420552+nousheeniqbal@users.noreply.github.com>
2025-02-12 00:23:07 +0500

    Update importSanityData.mjs

--- script/importSanityData.mjs
+++ script/importSanityData.mjs
@@ -11,19 +11,14 @@ dotenv.config({ path: path.resolve(__dirname, '../.env.local') });
 
 // Create Sanity client
 const client = createClient({
-  projectId:"j7dstq5e",
-  dataset:"production",
+  projectId:process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset:process.env.NEXT_PUBLIC_SANITY_DATASET,
   useCdn: false,
   apiVersion: '2025-01-13',
-  token:"skP0Kvt4DfBEWEfEWsS8WoRGv6i7n14jrfIOitcAcJOI2Amu2sYBltZEV9btnREIRpti7Gz3fGaCxARSknTHseXSjHUjNzLA9EyooFSkdIbdUYO4ZTEpprgsoibMRqMC353JuTfig0Jnw7tSh3udY49BsqmepXuLNlh1ycaRQiRbfkcD8PbY"
+  token:process.env.SANITY_API_TOKEN,
 })
 
-  // projectId:"48ced7dj",
-  // dataset: "production",
-  // useCdn: false,
-  // apiVersion: '2025-01-13',
-  // token: "sk6IImxyGrbUwYfGGZNjIiOWl88WIoktOmPejX8juk2zH5XQP0sgDkVDWZC9x9QYxn3vLlwLW6rdbgc6DETpa4q98VjX5ST1Oi6YEZbGjOcgA0ALWDPSSzq9jQMY4gRZfxW4tnK0UvuGjrLzQR8OzQDEixlitBZrlpXy1yvv4aI1H6VHXJzF"
-
+ 
 
 
 async function uploadImageToSanity(imageUrl) {
